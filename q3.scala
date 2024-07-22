1305a0ce305b14b0c30c8a6035fe32e43f22828b
Sushmitha0920 <140165054+Sushmitha0920@users.noreply.github.com>
2024-07-23 03:36:22 +0530

    Update q3.scala

--- q3.scala
+++ q3.scala
@@ -1,12 +1,10 @@
 object Fibonacci {
-  // Recursive function to calculate the nth Fibonacci number
   def fib(n: Int): Int = {
     if (n <= 0) 0
     else if (n == 1) 1
     else fib(n - 1) + fib(n - 2)
   }
 
-  // Recursive function to print the first n Fibonacci numbers
   def printFibonacci(n: Int): Unit = {
     def printFibonacciHelper(current: Int): Unit = {
       if (current < n) {
@@ -18,8 +16,8 @@ object Fibonacci {
   }
 
   def main(args: Array[String]): Unit = {
-    val n = 10 // Change this value to print more or fewer Fibonacci numbers
-    println(s"First $n Fibonacci numbers:")
+    val n = 10
+    println(s"First $n Fibonacci numbers : ")
     printFibonacci(n)
   }
 }
